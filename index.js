aa589c352b30dca2572fd96002545b1640e1d138
ARDoagooei <a.r.doagooei@gmail.com>
2025-03-06 12:43:52 +0100

    fixing the cors to handle post request

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-import express from "express";
-import cors from "cors"; // Import CORS
-import userRoutes from "./routes/userRoutes.js";
-import postRoutes from "./routes/postRoutes.js";
-import "./db/orm.js"; // Import for side effects (to connect to the database)
+import express from 'express';
+import cors from 'cors'; // Import CORS
+import userRoutes from './routes/userRoutes.js';
+import postRoutes from './routes/postRoutes.js';
+import './db/orm.js'; // Import for side effects (to connect to the database)
 
 // Create an express app
 const app = express();
 
 // Enable CORS for all routes
-app.use(cors()); // Allows all origins (you can specify a specific one if needed)
+// app.use(cors()); // Allows all origins (you can specify a specific one if needed)
 
+app.use(
+    cors({
+        origin: 'http://localhost:5173', // Allow only frontend requests
+        methods: 'GET,POST,PUT,DELETE', // Allow specific methods
+        allowedHeaders: 'Content-Type,Authorization', // Allow headers needed for requests
+    })
+);
 // Set a port from the environment variable or default to 8080
 const port = process.env.PORT || 8080;
 
@@ -17,8 +24,8 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 // // Routes
-app.use("/users", userRoutes);
-app.use("/posts", postRoutes);
+app.use('/users', userRoutes);
+app.use('/posts', postRoutes);
 
 // Start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
--- models/User.js
+++ models/User.js
@@ -1,6 +1,9 @@
 import { DataTypes } from 'sequelize';
 import sequelize from '../db/orm.js';
-
+//Since users table was created manually in the database , Sequelize will use it as long as the
+//  structure matches the model definition. In this case, Sequelize doesn't need to sync anything; 
+// it just maps the User model to the existing users tabl
+//Therefore, in the following, the user.sync is removed
 const User = sequelize.define(
     'User',
     {
