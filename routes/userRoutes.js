4595a57cdfd9a7b554f66df7fd9c10c3740de6dd
RajviMadhani <127208465+RajviMadhani@users.noreply.github.com>
2025-03-06 13:15:31 +0100

    Update blog page...

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,15 @@
-import express from 'express';
+import express from "express";
 import {
-    getUsers,
-    getUserById,
-    createUser,
-    updateUser,
-    deleteUser,
-} from '../controllers/userController.js';
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+} from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.route('/').get(getUsers).post(createUser);
-router.route('/:id').get(getUserById).put(updateUser).delete(deleteUser);
+router.route("/").get(getUsers).post(createUser);
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 export default router;
